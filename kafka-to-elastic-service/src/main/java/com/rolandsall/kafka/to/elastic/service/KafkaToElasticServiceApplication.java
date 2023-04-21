1bd5986e42543724a261e9b180370ba4136ede92
RolandSall <roland.salloum00@outlook.com>
2023-04-22 00:28:41 +0300

    feat: kafka consumer

---
+++ kafka-to-elastic-service/src/main/java/com/rolandsall/kafka/to/elastic/service/KafkaToElasticServiceApplication.java
@@ -0,0 +1,14 @@
+package com.rolandsall.kafka.to.elastic.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.rolandsall")
+public class KafkaToElasticServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
+    }
+}
+
